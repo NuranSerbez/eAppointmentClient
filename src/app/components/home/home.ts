7d8c5c1db02476da9d7c84571a525e9b11f4d8a5
NuranSerbez <nuranserbez1@gmail.com>
2025-10-20 15:01:43 +0300

    second-commit

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -4,6 +4,8 @@ import { DoctorModel } from '../../models/doctor.model';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DxSchedulerModule } from 'devextreme-angular';
+import { Http } from '../../services/http';
+import { AppointmentModel } from '../../models/appointment.model';
 
 @Component({
   selector: 'app-home',
@@ -17,23 +19,46 @@ export class Home {
   doctors: DoctorModel[] = [];
 
   selectedDepartmentValue: number = 0;
-  selectedDoctorId: string = '';
+  selectedDoctorId: string = "";
 
   appointments: any = [
     {
-      startDate: new Date('2024-04-11 09:00'),
-      endDate: new Date('2024-04-11 09:30'),
-      title: 'Nuran Serbez',
+      startDate: new Date("2024-04-11 09:00"),
+      endDate: new Date("2024-04-11 09:30"),
+      title: "Nuran Serbez",
     },
     {
-      startDate: new Date('2024-04-11 09:30'),
-      endDate: new Date('2024-04-11 10:00'),
-      title: 'Nuran Serbez',
+      startDate: new Date("2024-04-11 10:00"),
+      endDate: new Date("2024-04-11 10:30"),
+      title: "Nuran Serbez",
     },
     {
-      startDate: new Date('2024-04-11 11:30'),
-      endDate: new Date('2024-04-11 12:00'),
-      title: 'Nuran Serbez',
+      startDate: new Date("2024-04-11 11:00"),
+      endDate: new Date("2024-04-11 11:30"),
+      title: "Nuran Serbez",
     },
   ];
+
+ constructor(
+    private http: Http
+  ){}
+
+  getAllDoctor(){
+    this.selectedDoctorId = "";
+    if(this.selectedDepartmentValue > 0){
+      this.http.post<DoctorModel[]>("Appointments/GetAllDoctorByDepartment", 
+            {departmentValue: +this.selectedDepartmentValue}, (res)=> {
+        this.doctors = res.data;
+      });
+    }
+  }
+
+  getAllAppointments(){
+    if(this.selectedDoctorId){
+      this.http.post<AppointmentModel[]>("Appointments/GetAllByDoctorId", 
+            {doctorId: this.selectedDoctorId}, (res)=> {
+        this.appointments = res.data;
+      });
+    }
+  }
 }
